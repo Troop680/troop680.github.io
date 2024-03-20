2d0fca5b5e4e777e6fb480a33280a748b1b80ea9
dacoder <bobbye23899@gmail.com>
2024-03-20 08:28:57 -0700

    Update copyright.js

--- assets/js/copyright.js
+++ assets/js/copyright.js
@@ -1,1 +1,7 @@
+copyrightText = document.getElementById("copyright")
 
+const startYear = 1969
+const dateObject = new Date()
+const currentYear = dateObject.getFullYear()
+
+copyrightText.textContent = `&copy; ${startYear} - ${currentYear}
